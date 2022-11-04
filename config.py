a3ce706fb2168115617420bbbcf34b34d0cd7bc1
Artem Shatalov <105654276+Shatalov11@users.noreply.github.com>
2022-11-04 22:26:52 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,84 @@
+
+# Имя пользователя для логина
+username = 'tamila'
+
+# Пароль пользователя для логина
+password = '123456'
+
+# Данные для создания нового юзера
+created_user = {
+  "id": 0,
+  "username": "tamila1",
+  "firstName": "Tamila",
+  "lastName": "Valieva",
+  "email": "tamila@valieva.org",
+  "password": "zxcv123456",
+  "phone": "+12345678901",
+  "userStatus": 0
+}
+
+# Данные для обновления юзера
+updated_user = {
+  "id": 0,
+  "username": "tamila2",
+  "firstName": "Tamila2",
+  "lastName": "Valieva2",
+  "email": "tamila@valieva2.org",
+  "password": "asdf234567",
+  "phone": "+12345678902",
+  "userStatus": 0
+}
+
+# Данные для создания пользователей списком
+list_of_users = [
+  {
+    "id": 0,
+    "username": "victor",
+    "firstName": "Victor",
+    "lastName": "Victorov",
+    "email": "victor@victorov.org",
+    "password": "asd999",
+    "phone": "+12345678903",
+    "userStatus": 0
+  },
+  {
+    "id": 0,
+    "username": "victor2",
+    "firstName": "Victor2",
+    "lastName": "Victorov2",
+    "email": "victor2@victorov2.org",
+    "password": "zxc888",
+    "phone": "+12345678904",
+    "userStatus": 0
+  }
+]
+
+# Данные для создания ордера
+order = {
+  "id": 0,
+  "petId": 0,
+  "quantity": 0,
+  "shipDate": "0",
+  "status": "placed",
+  "complete": True
+}
+
+# Данные для добавления нового питомца
+new_pet = {
+  "id": 0,
+  "category": {
+    "id": 0,
+    "name": "Category"
+  },
+  "name": "Name",
+  "photoUrls": [
+    "None"
+  ],
+  "tags": [
+    {
+      "id": 0,
+      "name": "Tags"
+    }
+  ],
+  "status": "available"
+}
